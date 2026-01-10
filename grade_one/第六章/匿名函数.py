088f7a5a0e91c3d1ba6bb09fc9188373f0e40d49
raojiarui <799119163@qq.com>
2026-01-10 21:41:45 +0800

    完成lambda匿名函数的学习

---
+++ grade_one/第六章/匿名函数.py
@@ -0,0 +1,17 @@
+# 函数作为参数传递
+def test_func(compute):
+    result = compute(1,2)
+    print(result)
+
+def compute(x,y):
+    return x+y
+
+test_func(compute)
+
+'''
+    函数compute,作为参数,传入了test_func中使用
+    这是计算逻辑的传递，而非数据的传递
+'''
+
+# lambda匿名函数,无法二次使用
+test_func(lambda x,y:x+y)
