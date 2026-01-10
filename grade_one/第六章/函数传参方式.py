576e827b1165723300130a0f66adce71b10f06a0
raojiarui <799119163@qq.com>
2026-01-10 20:56:39 +0800

    函数进阶学习

--- grade_one/第六章/函数传参方式.py
+++ grade_one/第六章/函数传参方式.py
@@ -6,3 +6,28 @@ person_info('Tom',18,'男')
 
 # 关键字参数
 person_info(name='Tom',age=18,gender='男')
+person_info(age=19,name="Anny",gender='woman')
+
+# 关键字参数和位置参数结合
+person_info("Hilary",age=20,gender="女")
+
+# 缺省参数
+def person_info_1(name,age,gender="男"):
+    print(f"{name}的年龄为{age},他是{gender}")
+
+person_info_1("Tom",12)
+person_info_1("Anny",18,"女")
+
+# 不定长传参
+# 位置传递
+def person_info_2(*args):
+    print(f"args的类型为{type(args)}")
+    print(args)
+
+person_info_2("Tom",18,"woman")
+# 关键字传递
+def person_info_3(**kwargs):
+    print(f"args的类型为{type(kwargs)}")
+    print(kwargs)
+
+person_info_3(name="Tom",age=18,gender="男",id=100)
